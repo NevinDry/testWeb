564c159d685e5d419fe92bf4b66dc084b3d3a7f5
louisgrolleau <louis.grolleau@laposte.net>
2015-03-03 14:07:16 +0100

    html connection + inscription OK php OK

---
+++ traitement.php
@@ -0,0 +1,59 @@
+ <?php
+// On commence par récupérer les champs
+
+ // connexion à la base
+ $db = mysqli_connect('localhost', 'root', '', "testweb")  or die('Erreur de connexion '.mysql_error());
+ 
+ 
+if(isset($_POST['email'])){      
+	$adressemail=mysqli_real_escape_string($db,$_POST['email']);
+}
+else{
+	$adressemail="";
+}
+
+if(isset($_POST['pseudo'])){
+	$pseudo=mysqli_real_escape_string($db,$_POST['pseudo']);
+}
+else{
+	$pseudo="";
+}
+
+if(isset($_POST['adresse'])){
+	$adresse=mysqli_real_escape_string($db,$_POST['adresse']);
+}
+else{
+	$adresse="";
+}
+
+if(isset($_POST['password'])){
+	$motdepasse=mysqli_real_escape_string($db,$_POST['password']);
+}
+else{
+	$motdepasse="";
+}
+
+
+// On vérifie si les champs sont vides
+if(empty($adressemail) OR empty($pseudo) OR empty($adresse) OR empty($motdepasse)){
+    echo '<font color="red">Attention, seul champ vide !</font>';
+}
+
+// Aucun champ n'est vide, on peut enregistrer dans la table
+else{
+	
+	// sélection de la base  
+
+    
+    // on écrit la requête sql
+    $sql = 'INSERT INTO user(user_mail, user_pseudo, user_adresse, user_password) VALUES("'.$adressemail.'","'.$pseudo.'","'.$adresse.'","'.md5($motdepasse).'")';
+    
+    // on insère les informations du formulaire dans la table
+    $db->query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+
+    // on affiche le résultat pour le visiteur
+    echo 'Vos infos ont été ajoutées.';
+
+    mysqli_close($db);  // on ferme la connexion
+} 
+?> 
