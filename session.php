af71d4b6115c8801108d7ced0c4bb80eae34938e
puissae <puissae@FRMARCOG9447.mmpj.pernod-ricard.group>
2015-03-04 15:58:23 +0100

    Revert "Revert "css page ajout code barre""
    This reverts commit 3e58ad5ff5390cb6c7eea3f066259ab8f3480558.

--- session.php
+++ session.php
@@ -12,13 +12,13 @@
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<link rel="stylesheet" href="css/bootstrap.css"/>
 		<link rel="stylesheet" href="css/bootstrap-theme.css"/>
-		<h1>Espace membre</h1>
+		<h1 class="texte">Espace membre</h1>
 	</head>
 	
 		<body>
 			<div id="vosInformations">
-				<h4>Vos informations:</h4>
-					<div id="informationSession">
+				<h4 class="texte">Vos informations:</h4>
+					<div class="texte" id="informationSession">
 						<?php
 						echo ($_SESSION['pseudoConnexion']);
 						?>
@@ -30,6 +30,7 @@
 						<?php 
 						echo ($_SESSION['user_adresse']);
 						?>
+						<form method="link" action="codebarre.php"><div class="lineForm"><input type="submit" class="btn" id="boutonCodeBarre" value="Ajouter un code barre"></div></form>
 					</div>
 				</div>
 			<form action="deconnexion.php" method="post">	
